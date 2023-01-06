e0eb1f1e5c15646391813187758c56d9849d602e
djdeveloper22 <djdeveloper@gmail.com>
2023-01-06 10:44:08 -0500

    contador en las secciones de add task

--- src/app/core/utils/FunctionElementWeb.ts
+++ src/app/core/utils/FunctionElementWeb.ts
@@ -1,10 +1,8 @@
-import { TodoDto } from './../../domain/dto/TodoDto';
+import { TodoDto } from "../../domain/dto/TodoDto";
 
 export class FunctionElementWeb {
-
-  insertElementWeb(contador: number, elementos: Array<TodoDto>) {
-    const container__listTask = document.querySelector('.container__listTask');
-    container__listTask.innerHTML += `
+  insertListTask(contador: number, elementos: Array<TodoDto>, listTask: HTMLElement): void {
+    listTask.innerHTML += `
        <div class="elementos__listTask">
          <input class="checkbox-pop" type="checkbox" id="check${contador}" />
          <label for="check${contador}"><span></span> ${elementos[contador - 1].nameTask}</label>
@@ -12,8 +10,13 @@ export class FunctionElementWeb {
      `
   }
 
-  cleanForm(formulario: HTMLFormElement) {
+  cleanForm(formulario: HTMLFormElement): void {
     formulario.reset();
   }
 
+  isTypeProjects(v1: any, v2: HTMLFormElement, elemento: InnerHTML, sumador: number) {
+    if (v1 === v2.value)
+      console.log(elemento.innerHTML = `${sumador++}`);
+    
+  }
 }
--- src/app/ui/todoWeb/index.ts
+++ src/app/ui/todoWeb/index.ts
@@ -21,17 +21,16 @@ export class myWebApp implements IWebAplication {
       this.componenteWeb.setComponent('section_Icons', 'sectionIcon'),
         this.componenteWeb.setComponent('app_projects', 'sectionAppProjects'),
         this.componenteWeb.setComponent('app_design', 'sectionAppDesign')
-    }, 100)
+    }, 100);
     setTimeout(() => this.showModal(), 500);
   }
 
   showModal(): void {
-    const root = document.querySelector('#root');
-    const modal = document.createElement('div');
-    modal.className = 'modal';
-    modal.id = 'myModal';
-
     this.ev.click('#showModal', () => {
+      const root = document.querySelector('#root');
+      const modal = document.createElement('div');
+      modal.className = 'modal';
+      modal.id = 'myModal';
       root.appendChild(modal);
       setTimeout(() => this.componenteWeb.setComponent('myModal', 'modalTask'), 100);
       setTimeout(() => {
@@ -50,25 +49,30 @@ export class myWebApp implements IWebAplication {
       setTimeout(() => {
         modal.style.marginLeft = '-100%';
         modal.style.transition = 'all 0.5s ease-in'
-      }, 100)
-      setTimeout(() => modal.remove(), 700)
+      }, 100);
+      setTimeout(() => modal.remove(), 700);
     });
   }
 
   saveTask(): void {
     this.ev.click('#btnSave', () => {
       let acumulador: number = 0;
-      const cleanInput = new FunctionElementWeb();
-      const formulario: HTMLFormElement = document.querySelector('#formulario');
       const moduloTarea: HTMLFormElement = document.querySelector('#moduloTarea');
       const typeProject: HTMLFormElement = document.querySelector('#inputProject');
       const inputOptionPrioridad: HTMLFormElement = document.querySelector('#inputOptionPrioridad');
       const inputEstadoTask: HTMLFormElement = document.querySelector('#inputEstadoTask');
-      const contador = document.querySelector('#contadorTareas');
+      const formulario: HTMLFormElement = document.querySelector('#formulario');
+      let contador = document.querySelector('#contadorTareas');
+      const contTaskProject = document.querySelector('#contTaskProject');
+      let contTask: number = 1;
+      const contTaskDesign = document.querySelector('#contTaskDesign');
+      let contDesign: number = 1;
+      const contTaskOtros = document.querySelector('#contTaskOtros');
+      let contOtros: number = 1;
       const msjTask: HTMLElement = document.querySelector('#msjTarea');
       const title: HTMLElement = document.querySelector('.title');
-      title.style.display = 'none';
-
+      const elementWeb = new FunctionElementWeb();
+      //title.style.display = 'none';
       let taskIsSave = this.task.create({
         nameTask: moduloTarea.value,
         typeProjetc: typeProject.value,
@@ -79,29 +83,32 @@ export class myWebApp implements IWebAplication {
 
       if (taskIsSave) {
         contador.innerHTML = `${this.task.read().length}`
+        this.task.read().some(el=>{
+          if(el.typeProjetc === valueProject.project1)
+            contTaskProject.innerHTML = `${contTask++}`;
+          else if(el.typeProjetc === valueProject.project2)
+            contTaskDesign.innerHTML = `${contDesign++}`;
+          else
+          contTaskOtros.innerHTML = `${contOtros++}`;
+          elementWeb.isTypeProjects(el.typeProjetc, typeProject.value, contTaskProject, contTask);
+        });
+
         acumulador = acumulador + this.task.read().length;
         msjTask.style.top = '1rem';
-        msjTask.style.transition = 'all 0.3s ease-in'
+        msjTask.style.transition = 'all 0.3s ease-in';
         setTimeout(() => {
           msjTask.style.top = '-7rem';
-          this.showTask(acumulador);
+          this.showTask(acumulador, formulario);
         }, 1500);
       }
-
-       cleanInput.cleanForm(formulario);
     });
   }
 
-  showTask(contador: number): void {
+  showTask(contador: number, formulario: HTMLFormElement): void {
     const elementWeb = new FunctionElementWeb();
-    const inputProject: HTMLFormElement = document.querySelector('#inputProject');
+    const container__listTask: HTMLElement = document.querySelector('.listTask');
     let elementos = this.task.read();
-    if (elementos.length) {
-      // elementos.forEach((el, pos)=>{
-      //   console.log(el.nameTask + ' ' + inputProject.options[pos].value);
-        
-      // })
-      elementWeb.insertElementWeb(contador, elementos);
-    }
+    elementWeb.insertListTask(contador, elementos, container__listTask);
+    elementWeb.cleanForm(formulario);
   }
 }
